164f060b6d2128ba15b2f659363d0a9151cf2661
Mahesh-tec <80310305+Mahesh-tec@users.noreply.github.com>
2021-07-26 20:32:57 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer_bottom">	
+  <div class="container">
+   
+  	<div class="clearfix"> </div>
+	<div class="copy">
+		<p>Copyright © 2021 . All Rights Reserved . Design by <a href="http://www.aine.com/" target="_blank">AineSolutions.</a> </p>
+	</div>
+  </div>
+</div>
